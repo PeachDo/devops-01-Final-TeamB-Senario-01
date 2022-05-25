e4f85a725e4989779dadd2776cef332e41ae40f7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-05-25 01:18:21 +0000

    Fix:app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express') 
 const app = express() 
 const cors = require('cors');
-const port = 3000 
-const read = require('body-parser/lib/read');
+const { Producer } = require('sqs-producer');
+const port = 3000
+const bodyParser = require('body-parser')
 
+const producer = Producer.create({
+  queueUrl: 'https://sqs.ap-northeast-2.amazonaws.com/523139768306/EC2_SQS_ECS',
+  region: 'ap-northeast-2'
+});
 
-
+app.use(bodyParser.json())
 app.use(
   cors({
     origin: true,
@@ -15,20 +20,13 @@ app.use(
 
 app.get('/', (req, res) => res.send('Hello june World!')) 
 
-app.get('/api/get/june', function(req, res){
-    res.status(200).json({
-      "message" : "call get api june"
-    });
-  });
-  
 app.post('/api/post/june', function(req, res){
-
-    res.status(200).json({
-      "message" : "call post api june"
-    });
+    console.log(req.body)
+     producer.send(JSON.stringify(req.body))
+    res
+      module.exports = Producer.send
   });
   
-
 app.listen(port, () => {
   console.log(`서버가 ${port}번에서 작동중입니다.`);
 });
